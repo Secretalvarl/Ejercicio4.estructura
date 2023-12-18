0cbf53fef93e880febb1a61dfc847e9706ad443f
DESKTOP-R8KP20E\Profesor <csi23-agarera@colegioaltair.net>
2023-12-18 14:32:30 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio4.estructura/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ejercicio4.estructura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribame el tamaño del vector");
+            int tamanyo = Convert.ToInt32(Console.ReadLine());
+
+            int[] numeroAleatorio = new int[tamanyo];
+
+            Random voleo = new Random();
+
+            for (int i = 0; i < tamanyo; i++)
+            {
+                int numeroGuardar = voleo.Next(1, 1000);
+                numeroAleatorio[i] = numeroGuardar;
+
+                Console.WriteLine(i + ": " + numeroAleatorio[i]);
+            }
+
+            Console.WriteLine("" + "\n" + "");
+
+            Array.Sort(numeroAleatorio);
+
+            for (int j = 0; j < tamanyo; j++)
+            {
+                Console.WriteLine(j + ": " + numeroAleatorio[j]);
+            }
+
+        }
+    }
+}
